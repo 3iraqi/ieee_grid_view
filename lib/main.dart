bdb2530b7be6b4bd71d785d82d41141384edfd10
3iraqi <mohamed.8.eleraqi@gmail.com>
2022-09-08 18:10:03 +0200

    0000000

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-main()=>runApp(const GridView());
+main()=>runApp(const GridViewTask());
 
-class GridView extends StatelessWidget {
-  const GridView({Key? key}) : super(key: key);
+class GridViewTask extends StatelessWidget {
+  const GridViewTask({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) =>const MaterialApp(
+    debugShowCheckedModeBanner: false,
     home:  Home(),
   );
 }
@@ -14,7 +15,46 @@ class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) =>const Scaffold(
+  Widget build(BuildContext context) => Scaffold(
+
+    appBar: AppBar(
+      elevation: 0,
+      centerTitle: true,
+      title: const Text(
+          "Gallery",
+        style: TextStyle(
+          color: Colors.black54,
+        ),
+      ),
+    backgroundColor: Colors.white,
+    ),
+    body: GridView.count(
+        crossAxisCount:2,
+        crossAxisSpacing: 4,
+        mainAxisSpacing: 4,
+        childAspectRatio: .8,
+        // scrollDirection: Axis.horizontal,
+        children: List.generate(100, (index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+
+              decoration: BoxDecoration(
+              color: Colors.red,
+                borderRadius: BorderRadius.circular(20)
+              ),
+              child: Center(
+                child: Text(
+                  "item $index",
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+              ),
+            ),
+          );
+        }
+                  ),
+    ),
+
 
 
   );
